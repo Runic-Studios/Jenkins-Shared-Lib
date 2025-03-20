a549ec0127b9e3ee236906f0dc75dce246a5772c
Excel619 <excel8392@gmail.com>
2025-03-20 18:57:02 -0400

    Updated jenkinsAgent.groovy to only pass yaml block

--- vars/jenkinsAgent.groovy
+++ vars/jenkinsAgent.groovy
@@ -1,26 +1,22 @@
-def call() {
-    return {
-        kubernetes {
-            yaml """
-            apiVersion: v1
-            kind: Pod
-            spec:
-              containers:
-              - name: jenkins-agent
-                image: registry.runicrealms.com/jenkins-agent:latest
-                command:
-                - cat
-                tty: true
-                securityContext:
-                  privileged: true
-                volumeMounts:
-                - name: docker-sock
-                  mountPath: /var/run/docker.sock
-              volumes:
-              - name: docker-sock
-                hostPath:
-                  path: /var/run/docker.sock
-            """
-        }
-    }
+static def call() {
+    return """
+    apiVersion: v1
+    kind: Pod
+    spec:
+      containers:
+      - name: jenkins-agent
+        image: registry.runicrealms.com/jenkins-agent:latest
+        command:
+        - cat
+        tty: true
+        securityContext:
+          privileged: true
+        volumeMounts:
+        - name: docker-sock
+          mountPath: /var/run/docker.sock
+      volumes:
+      - name: docker-sock
+        hostPath:
+          path: /var/run/docker.sock
+    """
 }
