a0741b7cc6612691105af2680fe179cbed3085d3
Excel619 <excel8392@gmail.com>
2025-03-21 10:35:35 -0400

    Added search for docker in PATH for dockerbuildpush

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,6 +1,12 @@
 def call(String imageName, String tag, String registry) {
     withCredentials([usernamePassword(credentialsId: 'docker-registry-credentials', usernameVariable: 'REGISTRY_USER', passwordVariable: 'REGISTRY_PASSWORD')]) {
         sh """
+            echo "Checking for Docker CLI..."
+            echo "Current PATH: \$PATH"
+            which docker || echo "docker not found in PATH"
+            ls -l \$(which docker) || echo "docker binary not accessible"
+            docker version || echo "Docker CLI present but daemon may be inaccessible"
+    
             echo "Logging into registry..."
             echo $REGISTRY_PASSWORD | docker login ${registry} -u $REGISTRY_USER --password-stdin
 
