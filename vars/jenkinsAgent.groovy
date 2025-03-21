7c9efa5d757ca0775c7c2cf46b7bd284c8fba5f0
Excel619 <excel8392@gmail.com>
2025-03-20 23:46:10 -0400

    Added configuration to create registry credential

--- vars/jenkinsAgent.groovy
+++ vars/jenkinsAgent.groovy
@@ -1,22 +1,52 @@
 static def call() {
-    return """
-    apiVersion: v1
-    kind: Pod
-    spec:
-      containers:
-      - name: jenkins-agent
-        image: registry.runicrealms.com/jenkins-agent:latest
-        command:
-        - cat
-        tty: true
-        securityContext:
-          privileged: true
-        volumeMounts:
-        - name: docker-sock
-          mountPath: /var/run/docker.sock
-      volumes:
-      - name: docker-sock
-        hostPath:
-          path: /var/run/docker.sock
-    """
+    def podYaml = ''
+
+    withCredentials([usernamePassword(credentialsId: 'docker-registry-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        def dockerCfg = """
+        {
+          "auths": {
+            "registry.runicrealms.com": {
+              "username": "${DOCKER_USER}",
+              "password": "${DOCKER_PASS}",
+              "auth": "${"${DOCKER_USER}:${DOCKER_PASS}".bytes.encodeBase64().toString()}"
+            }
+          }
+        }
+        """.stripIndent().trim()
+
+        writeFile file: 'docker-config.json', text: dockerCfg
+
+        sh '''
+          kubectl delete secret regcred -n jenkins --ignore-not-found
+          kubectl create secret generic regcred \
+            --from-file=.dockerconfigjson=docker-config.json \
+            --type=kubernetes.io/dockerconfigjson \
+            -n jenkins
+        '''
+
+        podYaml = """
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: jenkins-agent
+            image: registry.runicrealms.com/jenkins-agent:latest
+            command:
+            - cat
+            tty: true
+            securityContext:
+              privileged: true
+            volumeMounts:
+            - name: docker-sock
+              mountPath: /var/run/docker.sock
+          imagePullSecrets:
+          - name: regcred
+          volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+        """
+    }
+
+    return podYaml
 }
