68ce359a28e68d47c2464d4ec3ef74de2144ae6e
Excel619 <excel8392@gmail.com>
2025-03-20 23:49:45 -0400

    Updated jenkinsAgent to inject steps

--- vars/jenkinsAgent.groovy
+++ vars/jenkinsAgent.groovy
@@ -1,24 +1,30 @@
-static def call() {
+def call(steps) {
     def podYaml = ''
 
-    withCredentials([usernamePassword(credentialsId: 'docker-registry-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    steps.withCredentials([
+            steps.usernamePassword(
+                    credentialsId: 'docker-registry-credentials',
+                    usernameVariable: 'DOCKER_USER',
+                    passwordVariable: 'DOCKER_PASS'
+            )
+    ]) {
         def dockerCfg = """
         {
-          "auths": {
-            "registry.runicrealms.com": {
-              "username": "${DOCKER_USER}",
-              "password": "${DOCKER_PASS}",
-              "auth": "${"${DOCKER_USER}:${DOCKER_PASS}".bytes.encodeBase64().toString()}"
+            "auths": {
+                "registry.runicrealms.com": {
+                    "username": "${DOCKER_USER}",
+                    "password": "${DOCKER_PASS}",
+                    "auth": "${"${DOCKER_USER}:${DOCKER_PASS}".bytes.encodeBase64().toString()}"
+                }
             }
-          }
         }
         """.stripIndent().trim()
 
-        writeFile file: 'docker-config.json', text: dockerCfg
+        steps.writeFile file: 'docker-config.json', text: dockerCfg
 
-        sh '''
-          kubectl delete secret regcred -n jenkins --ignore-not-found
-          kubectl create secret generic regcred \
+        steps.sh '''
+        kubectl delete secret regcred -n jenkins --ignore-not-found
+        kubectl create secret generic regcred \
             --from-file=.dockerconfigjson=docker-config.json \
             --type=kubernetes.io/dockerconfigjson \
             -n jenkins
