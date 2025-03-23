31518b69986556d466b62b9bf75bd97c63ea6b40
Excel619 <excel8392@gmail.com>
2025-03-22 23:03:02 -0400

    Changed updateDeployment -> updateManifest

---
+++ vars/updateManifest.groovy
@@ -0,0 +1,22 @@
+def call(String branch, String repository, String repositoryPath, String imageName, String tag, String registry, String registryProject) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'github-ssh', keyFileVariable: 'SSH_KEY')]) {
+        sh """
+            rm -rf ${repository}
+            export GIT_SSH_COMMAND='ssh -i $SSH_KEY -o StrictHostKeyChecking=no'
+            git clone --branch ${branch} git@github.com:Runic-Studios/${repository}.git ${repository}
+
+            echo "Updating ${repositoryPath}..."
+            yq eval -i '(.. | select(has("name") and .name == "${registry}/${registryProject}/${imageName}").newTag) = "${tag}"' ${repository}/${repositoryPath}
+
+            git config --global user.email "runicrealms.mc@gmail.com"
+            git config --global user.name "RunicRealmsGithub"
+
+            cd ${repository}
+            git add -A
+            git commit -m "Update ${imageName} newTag to ${tag}" || true
+            git push origin ${branch}
+
+            rm -rf ${repository}
+        """
+    }
+}
