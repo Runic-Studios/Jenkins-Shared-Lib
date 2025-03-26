5ac70725d664ceb68fffc11b1b6e525e589d8b11
Excel619 <excel8392@gmail.com>
2025-03-25 21:34:14 -0400

    Updated dockerBuildPush to include dockerfile argument

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,11 +1,11 @@
-def call(String imageName, String tag, String registry, String registryProject) {
+def call(String dockerfile, String imageName, String tag, String registry, String registryProject) {
     withCredentials([usernamePassword(credentialsId: 'docker-registry-credentials', usernameVariable: 'REGISTRY_USER', passwordVariable: 'REGISTRY_PASSWORD')]) {
         sh """
             echo "Logging into registry..."
             echo $REGISTRY_PASSWORD | docker login ${registry} -u '$REGISTRY_USER' --password-stdin
 
             echo "Building Docker image..."
-            docker build -t ${registry}/${registryProject}/${imageName}:${tag} .
+            docker build -f ${dockerfile} -t ${registry}/${registryProject}/${imageName}:${tag} .
 
             echo "Pushing Docker image..."
             docker push ${registry}/${registryProject}/${imageName}:${tag}
