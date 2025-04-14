ace1a7ab59112f7bde3ac09ee9ad29eac8251372
Excel619 <excel8392@gmail.com>
2025-04-14 10:35:40 -0600

    Changed jenkinsAgent to allow multiple agents

--- vars/jenkinsAgent.groovy
+++ vars/jenkinsAgent.groovy
@@ -1,25 +1,30 @@
-static def call(String image) {
-    return """
-    apiVersion: v1
-    kind: Pod
-    spec:
-      containers:
-      - name: jenkins-agent
-        image: ${image}
-        command:
-        - cat
-        tty: true
-        securityContext:
-          privileged: true
-          runAsUser: 0
-        volumeMounts:
-        - name: docker-sock
-          mountPath: /var/run/docker.sock
-      imagePullSecrets:
-      - name: regcred
-      volumes:
-      - name: docker-sock
-        hostPath:
-          path: /var/run/docker.sock
-    """
+static def call(Map<String, String>... containers) {
+    def containerYaml = containers.collect { c ->
+        """\
+  - name: ${c.key}
+    image: ${c.value}
+    command:
+    - cat
+    tty: true
+    securityContext:
+      privileged: true
+      runAsUser: 0
+    volumeMounts:
+    - name: docker-sock
+      mountPath: /var/run/docker.sock
+"""
+    }.join("")
+
+    return """\
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+${containerYaml}  imagePullSecrets:
+  - name: regcred
+  volumes:
+  - name: docker-sock
+    hostPath:
+      path: /var/run/docker.sock
+""".stripIndent()
 }
